fdc70aba856c3eb1a91db1c4a154df70eaff03fc
EstudosJenifer <estudosjenifer2023@gmail.com>
2023-11-14 15:17:24 -0300

    Primeiro Desafio DIO Felipão

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-Jenifer teste inicial
+
+// por padrão vem na branch de master
+// quando quiser saber a url é só copiar das descrições ou do indicativo que está depois de criar o repositório
+// se modificar o projeto para enviar para o git tenho que dar o comando git add . ou o nome do arquivo
+// depois do add, executar git commit -m "" ( para escrever alguma mensagem desejada)
+// e para efetuar realmente a modificação executar git push -u origin main
+// 1) git add .
+// 2) git commit -m "adicionei o texto"
+// 3) git push -u origin main
+// se modifiquei, exclui ou estou em outra máquina e não tenho mais o projeto local
+// para se recuperar o projeto que ta no git ir no perfil, repositório desejado
+// clicar em Code e copiar ou clonar o endereço do repositório, criar a pasta projeto ou selecionar a pasta
+// abrir o terminal e executar o comando git clone _______(url)
+
+
+let nome = "Jenifer"
+console.log(nome)
