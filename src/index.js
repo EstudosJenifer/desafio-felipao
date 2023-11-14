effc867bdf77b7383ec1fa6b06e0bd646e1080f5
EstudosJenifer <estudosjenifer2023@gmail.com>
2023-11-14 13:35:30 -0300

    adicionei o texto

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 // se modificar o projeto para enviar para o git tenho que dar o comando git add . ou o nome do arquivo
 // depois do add, executar git commit -m "" ( para escrever alguma mensagem desejada)
 // e para efetuar realmente a modificação executar git push -u origin main
+// 1) git add .
+// 2) git commit -m "adicionei o texto"
+// 3) git push -u origin main
 
 let nome = "Jenifer"
 console.log(nome)
