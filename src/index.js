40db481a67dbdd00ad18b912f409ea4c45ae10ef
EstudosJenifer <estudosjenifer2023@gmail.com>
2023-11-14 13:27:34 -0300

    meu segundo commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+// por padrão vem na branch de master
+// quando quiser saber a url é só copiar das descrições ou do indicativo que está depois de criar o repositório
+// se modificar o projeto para enviar para o git tenho que dar o comando git add . ou o nome do arquivo
+
+console.log("OI")
