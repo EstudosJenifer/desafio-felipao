39889cff12a821056e81936229a28b4fc2e9f024
EstudosJenifer <estudosjenifer2023@gmail.com>
2023-11-14 13:33:46 -0300

    adicionei o texto

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // por padrão vem na branch de master
 // quando quiser saber a url é só copiar das descrições ou do indicativo que está depois de criar o repositório
 // se modificar o projeto para enviar para o git tenho que dar o comando git add . ou o nome do arquivo
+// depois do add, executar git commit -m "" ( para escrever alguma mensagem desejada)
+// e para efetuar realmente a modificação executar git push -u origin main
 
-console.log("OI")
+let nome = "Jenifer"
+console.log(nome)
